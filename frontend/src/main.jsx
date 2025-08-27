13b5f1db6f5321574de8aea2a94386a9c4c3f44f
jungsday2 <jungseo0412@gmail.com>
2025-08-28 01:30:33 +0900

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App.jsx'
 import './index.css' // 기본 스타일링을 위해 CSS 파일도 추가합니다.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
